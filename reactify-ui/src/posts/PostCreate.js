d40888c949e54abe50c88edbd9fbd061bdee1bcb
Varun Walia <varunwalia5400@gmail.com>
2018-07-22 23:46:12 +0530

    Intial Commit

---
+++ reactify-ui/src/posts/PostCreate.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+import PostForm from './PostForm'
+
+class PostCreate extends Component {
+  render(){
+    return (
+      <div>
+      <h1>Create Post</h1>
+        <PostForm />
+      </div>
+    )
+  }
+
+}
+
+export default PostCreate
+
